c2b3d40ddc80e603f968fcdd5cb4dba37b8f23e7
Gulasal <macbook@Gulasal.local>
2025-01-25 18:17:20 +0600

    Drivers

---
+++ src/test/java/talent_lms/drivers/FireFoxWebDriver.java
@@ -0,0 +1,17 @@
+package talent_lms.drivers;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.time.Duration;
+
+public class FireFoxWebDriver {
+    public static WebDriver loadFireFox(){
+        WebDriverManager.firefoxdriver().setup();
+        WebDriver driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        return driver;
+    }
+}
