be5c8f99d3e236b85e5bf8e255d613384190642b
Saida77R <saida.rust77@gmail.com>
2025-01-31 09:10:14 +0600

    saida_branch/Groups

---
+++ src/main/java/com/digital_nomads/talent_lms/entity/Groups.java
@@ -0,0 +1,24 @@
+package com.digital_nomads.talent_lms.entity;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
+
+/**
+ * @author Saida
+ */
+
+
+public class Groups {
+
+    private String name = "Fall24";
+    private String description = "We are family";
+
+
+}
