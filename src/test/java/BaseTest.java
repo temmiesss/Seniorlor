b07f5674db1607f603aacf330c99cb0e56af078f
Saida77R <saida.rust77@gmail.com>
2025-01-31 09:24:19 +0600

    changing/page/BaseTest/saida_branch

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,7 +1,10 @@
 import com.digital_nomads.talent_lms.drivers.Driver;
+import com.digital_nomads.talent_lms.entity.Groups;
 import com.digital_nomads.talent_lms.helper.AlertHelper;
 import com.digital_nomads.talent_lms.helper.WebElementActions;
+import com.digital_nomads.talent_lms.page.addGroup.AddGroupPage;
 import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeSuite;
 import com.digital_nomads.talent_lms.page.login.LoginPage;
 import com.digital_nomads.talent_lms.page.users.UserPage;
@@ -9,13 +12,15 @@ import com.digital_nomads.talent_lms.utils.randomEntityUtils.RandomUserGenerator
 import page.DeleteCourse;
 
 public abstract class BaseTest {
-    public static WebDriver driver;
+    public WebDriver driver;
     WebElementActions webElementActions = new WebElementActions();
     AlertHelper alertHelper;
     UserPage userPage = new UserPage();
     LoginPage loginPage = new LoginPage();
     RandomUserGenerator randomUserGenerator = new RandomUserGenerator();
-    DeleteCourse deleteCourse = new DeleteCourse(driver);    // Rano added
+    DeleteCourse deleteCourse = new DeleteCourse(driver);
+    AddGroupPage addGroupPage = new AddGroupPage();
+    Groups group = new Groups();
 
 
 
@@ -23,6 +28,13 @@ public abstract class BaseTest {
     public void beforeSuite(){
         driver = Driver.getDriver();
         alertHelper = new AlertHelper(driver);
-
     }
+
+    @AfterClass
+    public void tearDown() {
+        if (driver != null) {
+            driver.close();
+        }
+    }
+
 }
