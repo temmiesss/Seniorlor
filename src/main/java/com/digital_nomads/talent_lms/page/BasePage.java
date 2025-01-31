1e7474226c044f10ce467d0a1bd107589abadc41
Saida77R <saida.rust77@gmail.com>
2025-01-31 09:21:35 +0600

    changing/page/BasePage/saida_branch

--- src/main/java/com/digital_nomads/talent_lms/page/BasePage.java
+++ src/main/java/com/digital_nomads/talent_lms/page/BasePage.java
@@ -2,6 +2,7 @@ package com.digital_nomads.talent_lms.page;
 
 import com.digital_nomads.talent_lms.drivers.Driver;
 import com.digital_nomads.talent_lms.helper.WebElementActions;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.PageFactory;
 
 public class BasePage {
