9e2141bd7e5a3bdaa75eb346e2220aecd0a8de7b
Rano <ranotoktorbaeva@gmail.com>
2025-02-03 10:52:56 +0600

    Add some info/Rano/BaseTest

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,12 +1,16 @@
 import com.digital_nomads.talent_lms.drivers.Driver;
 import com.digital_nomads.talent_lms.helper.AlertHelper;
 import com.digital_nomads.talent_lms.helper.WebElementActions;
+import com.digital_nomads.talent_lms.page.course.CloneCoursePage;
+import com.digital_nomads.talent_lms.page.course.DeleteCourse;
+import com.digital_nomads.talent_lms.page.course.UpdateCourse;
+import com.digital_nomads.talent_lms.utils.randomEntityUtils.RandomCourseGenerator;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.BeforeSuite;
 import com.digital_nomads.talent_lms.page.login.LoginPage;
 import com.digital_nomads.talent_lms.page.users.UserPage;
 import com.digital_nomads.talent_lms.utils.randomEntityUtils.RandomUserGenerator;
-import page.DeleteCourse;
+
 
 public abstract class BaseTest {
     public static WebDriver driver;
@@ -15,7 +19,10 @@ public abstract class BaseTest {
     UserPage userPage = new UserPage();
     LoginPage loginPage = new LoginPage();
     RandomUserGenerator randomUserGenerator = new RandomUserGenerator();
-    DeleteCourse deleteCourse = new DeleteCourse(driver);    // Rano added
+    DeleteCourse deleteCourse = new DeleteCourse();    // Rano added
+    CloneCoursePage cloneCoursePage = new CloneCoursePage();
+    UpdateCourse updateCourse = new UpdateCourse();
+    RandomCourseGenerator randomCourseGenerator =new RandomCourseGenerator();
 
 
 
