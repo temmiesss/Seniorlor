a0ca0a1c1c0d3c68a4a70103c396772feadd73e0
Saida77R <saida.rust77@gmail.com>
2025-02-10 10:51:49 +0600

    java/com/digital_nomads/talent_lms/helper/WebElementActions.java

--- src/main/java/com/digital_nomads/talent_lms/helper/WebElementActions.java
+++ src/main/java/com/digital_nomads/talent_lms/helper/WebElementActions.java
@@ -1,15 +1,18 @@
 package com.digital_nomads.talent_lms.helper;
 
 import com.digital_nomads.talent_lms.drivers.Driver;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
 
 import static com.digital_nomads.talent_lms.drivers.Driver.driver;
 
@@ -96,6 +99,10 @@ public class WebElementActions {
     public WebElementActions clear(WebElement element) {
         element.clear();
         return this;
+    }
+
+    public boolean isDisplayed(WebElement element){
+       return element.isDisplayed();
     }
 
 
