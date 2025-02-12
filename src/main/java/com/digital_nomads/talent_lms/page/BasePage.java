41c70a50431dde0e5e70f36489d316947b68e64c
Amal <uwgating@gmail.com>
2025-02-12 15:48:29 +0600

    CategoryTests

--- src/main/java/com/digital_nomads/talent_lms/page/BasePage.java
+++ src/main/java/com/digital_nomads/talent_lms/page/BasePage.java
@@ -3,6 +3,7 @@ package com.digital_nomads.talent_lms.page;
 import com.digital_nomads.talent_lms.helper.WebElementActions;
 import com.digital_nomads.talent_lms.drivers.Driver;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
@@ -14,6 +15,7 @@ public class BasePage {
     public WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(10));
     public Random random = new Random();
     public WebDriver driver;
+    public Actions actions = new Actions(Driver.getDriver());
 
     public BasePage() {
         PageFactory.initElements(Driver.getDriver(), this);
--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -19,6 +19,8 @@ import com.digital_nomads.talent_lms.page.login.LoginPage;
 import com.digital_nomads.talent_lms.page.users.UserPage;
 import com.digital_nomads.talent_lms.utils.randomEntityUtils.RandomUserGenerator;
 
+import java.util.Random;
+
 
 public abstract class BaseTest {
     public WebDriver driver;
@@ -46,11 +48,11 @@ public abstract class BaseTest {
         alertHelper = new AlertHelper(driver);
     }
 
-    @AfterClass
-    public void tearDown() {
-        if (driver != null) {
-            driver.close();
-        }
-    }
+//    @AfterClass
+//    public void tearDown() {
+//        if (driver != null) {
+//            driver.close();
+//        }
+//    }
 
 }
