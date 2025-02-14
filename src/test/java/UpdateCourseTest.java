98ad4096db44ddc9091f60b6afa769802570bbe9
Rano <ranotoktorbaeva@gmail.com>
2025-02-14 16:12:34 +0600

    Fix/addAfterMethod

--- src/test/java/UpdateCourseTest.java
+++ src/test/java/UpdateCourseTest.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import java.time.Duration;
@@ -183,9 +184,10 @@ public class UpdateCourseTest extends BaseTest {
 
     }
 
-
-
-
+    @AfterMethod
+    public void tearDown() {
+        driver.manage().deleteAllCookies();
+    }
 }
 
 //10
