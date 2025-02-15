207fc14843c498cbb7438002c70981d392be1488
Aigerim Sardarbekova <aigerims@MacBook-Air-Aigerim-2.local>
2025-02-15 21:58:25 +0600

    gera/com # This is a combination of 2 commits.
    gera/com
    
    gerrr

--- src/test/java/AddNewCourseTest.java
+++ src/test/java/AddNewCourseTest.java
@@ -6,15 +6,14 @@ import com.digital_nomads.talent_lms.page.dashboard.DashboardPage;
 import com.digital_nomads.talent_lms.page.login.LoginPage;
 import com.digital_nomads.talent_lms.entity.Course;
 
+import org.aspectj.lang.annotation.Before;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 import com.digital_nomads.talent_lms.utils.randomEntityUtils.RandomCourseGenerator;
 
 import java.time.Duration;
